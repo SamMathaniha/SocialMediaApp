afe942d4b10285eab0a7e02c285bb2a42856c412
SamMathaniha <mathanihasam@gmail.com>
2024-06-05 00:32:01 +0530

    Added CSS styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import { useState } from "react";
-import Header from "./Components/Header";
-import Nav from "./Components/Nav";
+import Header from "./Header";
+import Nav from "./Nav";
 
 function App() {
+  const [posts, setPost] = useState([
+
+    {
+      id:1,
+      title:"My first post",
+      datatime: "Feb 20, 2024 12:01:01 AM",
+      body:"Happy birthday Sam"
+    },
+    {
+      id:2,
+      title:"My 2nd post",
+      datatime: "Feb 21, 2024 01:02:05 AM",
+      body:"I'm Coding"
+    },
+    {
+      id:3,
+      title:"My 3nd post",
+      datatime: "Mar 25, 2024 06:02:05 PM",
+      body:"I'm Eating"
+    }
+  ])
+
   const [search, setSearch]= useState('')
+  const [searchResults, setSearchResults] = useState([])
+
   return (
     <div className="App">
 
