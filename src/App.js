e0d36b86bbdce28adb118efb786274cb586a7c64
SamMathaniha <mathanihasam@gmail.com>
2024-06-04 23:31:45 +0530

    Updated Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-    
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header><h1>Header Hello</h1> </header>
+  )
+}
+
+export default Header
