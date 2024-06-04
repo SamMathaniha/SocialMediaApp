4f48b188a46313ac4be950bddf584844c3c47e09
SamMathaniha <mathanihasam@gmail.com>
2024-06-04 23:35:45 +0530

    Created Nav Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Header";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="App">
       <Header title="Social Media App"/>
+      <Nav />
     </div>
   );
 }
