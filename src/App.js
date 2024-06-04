d73a1a222779243d5bf3629b0932a4757416a431
SamMathaniha <mathanihasam@gmail.com>
2024-06-04 23:48:33 +0530

    Added useState for add in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Header from "./Components/Header";
 import Nav from "./Components/Nav";
 
 function App() {
+  const [search, setSearch]= useState('')
   return (
     <div className="App">
+
       <Header title="Social Media App"/>
-      <Nav />
+      <Nav 
+        search={search}
+        setSearch={setSearch}
+      />
     </div>
   );
 }
